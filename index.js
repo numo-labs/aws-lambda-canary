49c359440384ff94ccbd52da0ca6096d30df91e0
Jimmy Ruts <jimmy.ruts@thomascookonline.com>
2016-01-21 17:14:36 +0000

    Removed unneeded lines

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ var config = require('./lib/config');
 exports.handler = function (event, context) {
   config.init(context, function (err, config) {
     if (err) context.fail(err);
-
-    console.log(config);
     context.succeed(starwars());
   });
 }
-
-
-var result = values.sort((a, b) => a - b);
