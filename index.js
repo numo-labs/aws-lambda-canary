37e671790b7ab345abcc3abcac6c7697312be1c6
Jimmy Ruts <jimmy.ruts@thomascookonline.com>
2016-01-21 17:11:46 +0000

    Changed config

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var starwars = require('starwars');
 var config = require('./lib/config');
 
 exports.handler = function (event, context) {
-  // Check config
-  config.init(context, function (config) {
+  config.init(context, function (err, config) {
+    if (err) context.fail(err);
+
     console.log(config);
     context.succeed(starwars());
   });
 }
+
+
+var result = values.sort((a, b) => a - b);
