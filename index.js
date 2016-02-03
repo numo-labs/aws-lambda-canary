5cd5d391972af228738d0c3eedf9c7b4a1c1dbf3
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-02-03 10:13:51 +0000

    adding event emitter with context

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var config = require('./lib/config');
 var starwarsService = require ('./lib/starwars-service');
+var contextEmitter = require('./lib/context-emitter');
+
+
 
 exports.handler = function (event, context) {
+  contextEmitter.init(context);
   config.init(context, function (err, config) {
     if (err) context.fail(err);
-    context.succeed(starwarsService.quote);
   });
 }
