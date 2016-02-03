950f4293f220dc2c855285679f3499337ac5c7e4
Jimmy Ruts <jimmy.ruts@thomascookonline.com>
2016-02-03 10:04:57 +0000

    Add event emitter

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var starwars = require('starwars');
 var config = require('./lib/config');
+var starwarsService = require ('./lib/starwars-service');
 
 exports.handler = function (event, context) {
   config.init(context, function (err, config) {
     if (err) context.fail(err);
-    context.succeed(starwars());
+    context.succeed(starwarsService.quote);
   });
 }
