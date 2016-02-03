eefde642484777d495381439a59c73a4a2566297
Jimmy Ruts <jimmy.ruts@thomascookonline.com>
2016-02-03 10:35:23 +0000

    Added more logging

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var config = require('./lib/config');
 var starwarsService = require ('./lib/starwars-service');
 var contextEmitter = require('./lib/context-emitter');
 
-
-
 exports.handler = function (event, context) {
+  console.log('starting canary lambda');
   contextEmitter.init(context);
   // config.init(context, function (err, config) {
   //   if (err) context.fail(err);
