ac50acb087c5fc2fd4ef4bda37a9ab6764d23f08
Nikhila Ravi <nikkiravi92@gmail.com>
2016-02-03 17:01:12 +0000

    Rewrites test in es5 to try and debug

--- test/index.test.js
+++ test/index.test.js
@@ -3,22 +3,21 @@ import assert from 'assert';
 import index from '../index.js';
 import contextCreator from './utils/mockContext.js';
 
-describe('Handler:', () => {
-  it('Calls context.succeed with quote', done => {
-    const test = (result) => {
-      console.log("RESULT", result)
+describe('Handler:', function() {
+  it('Calls context.succeed with quote', function (done) {
+    function test (result) {
       assert(result.length > 5);
       done();
     };
-    const context = contextCreator(test);
+    var context = contextCreator(test);
     index.handler({}, context);
   });
-  it('Calls context:fail with error when no functionARN specified', done => {
-    const test1 = (error) => {
+  it('Calls context:fail with error when no functionARN specified', function (done) {
+    function test (error) {
       assert.equal(error, 'Environments are not correctly configured.');
       done();
     };
-    let context1 = contextCreator(test1);
+    var context1 = contextCreator(test);
     delete context1.invokedFunctionArn;
     index.handler({}, context1);
   });
