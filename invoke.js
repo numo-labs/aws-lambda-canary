a696ed222b7b14b8067aa53e83a1881274edd0cb
nelsonic <contact.nelsonic+github@gmail.com>
2016-02-23 10:16:19 +0000

    separate the (test) invocation of the lambda service from the deployment >> invoke.js https://github.com/numo-labs/aws-lambda-canary/issues/28

--- invoke.js
+++ invoke.js
@@ -1,33 +1,19 @@
 var AWS = require('aws-sdk');
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var zip = require('gulp-zip');
-var install = require('gulp-install');
-var runSequence = require('run-sequence');
-var packageJson = require('./package.json');
-var region = 'eu-west-1';
-var fs = require('fs');
-
-var functionName = packageJson.name + '-v' + getMajorVersion(packageJson.version);
+AWS.config.region = process.env.AWS_REGION;
+var lambda = new AWS.Lambda();
+var pkg = require('./package.json');
 
 function getMajorVersion (version) {
   return version.substring(0, version.indexOf('.'));
 }
 
-var outputName = packageJson.name + '.zip';
-
-// var IAMRole = process.env.IAM_ROLE;
-
-var filesToPack = ['./index.js', './lib/**/*.*'];
-
 function testInvoke () {
-  var lambda = new AWS.Lambda();
-
+  var functionName = pkg.name + '-v' + getMajorVersion(pkg.version)
   var params = {
     FunctionName: functionName,
     InvocationType: 'RequestResponse',
     LogType: 'Tail',
-    Payload: '{ "key1" : "name" }',
+    Payload: '{ "isHuman" : true }',
     Qualifier: 'ci' // see: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Lambda.html#invoke-property
   };
 
@@ -43,3 +29,7 @@ function testInvoke () {
     });
   }
 }
+
+module.exports = testInvoke;
+
+testInvoke();
